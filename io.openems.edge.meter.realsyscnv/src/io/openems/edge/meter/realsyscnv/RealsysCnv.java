f09dc21ddf3ccb0577205bb6982f032aca6ccf90
ganadara135 <ganadara135@gmail.com>
2020-11-01 15:00:52 +0900

    Done RealSys Converter with Enbac Fluid

--- io.openems.edge.meter.realsyscnv/src/io/openems/edge/meter/realsyscnv/RealsysCnv.java
+++ io.openems.edge.meter.realsyscnv/src/io/openems/edge/meter/realsyscnv/RealsysCnv.java
@@ -18,6 +18,7 @@ import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
 import io.openems.edge.bridge.modbus.api.BridgeModbus;
 import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
 import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.FloatDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedQuadruplewordElement;
@@ -44,15 +45,23 @@ public class RealsysCnv extends AbstractOpenemsModbusComponent
 	private Config config = null;
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId { 
+		MASS_FLOW_RATE(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)),
+		VOLUME_FLOW_RATE(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)), //
+		ENERGY_FLOW_RATE(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)),		
+		NORMAL_FLOW_RATE(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)),
 		PRESSURE(Doc.of(OpenemsType.FLOAT) //
-				.unit(Unit.NONE)), //
-		MASS_FLOW(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.NONE)),
-		VOLUME_FLOW(Doc.of(OpenemsType.FLOAT) //
-				.unit(Unit.NONE)), //
-		ENERGY_FLOW(Doc.of(OpenemsType.FLOAT) //
+		TOTAL_MASS_FLOW(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.NONE)),
-		NORMAL_FLOW(Doc.of(OpenemsType.FLOAT) //
+		TOTAL_VOLUME_FLOW(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)), //
+		TOTAL_ENERGY_FLOW(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE)),		
+		TOTAL_NORMAL_FLOW(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.NONE));
 
 		private final Doc doc;
@@ -104,47 +113,66 @@ public class RealsysCnv extends AbstractOpenemsModbusComponent
 //		0x1088 TOTAL ENERGY FLOW MJ
 //		0x108C TOTAL NORMAL FLOW Nm3 
 		return new ModbusProtocol(this,
+				new FC4ReadInputRegistersTask(0x106C, Priority.LOW,
+						m(RealsysCnv.ChannelId.MASS_FLOW_RATE, new FloatDoublewordElement(0x106C))),
+				new FC4ReadInputRegistersTask(0x1070, Priority.LOW,
+						m(RealsysCnv.ChannelId.VOLUME_FLOW_RATE, new FloatDoublewordElement(0x1070))),
+				new FC4ReadInputRegistersTask(0x1074, Priority.LOW,
+						m(RealsysCnv.ChannelId.ENERGY_FLOW_RATE, new FloatDoublewordElement(0x1074))),
+				new FC4ReadInputRegistersTask(0x1078, Priority.LOW,
+						m(RealsysCnv.ChannelId.NORMAL_FLOW_RATE, new FloatDoublewordElement(0x1078))),
+				
 				new FC4ReadInputRegistersTask(0x107C, Priority.LOW,
-						m(RealsysCnv.ChannelId.PRESSURE, new UnsignedQuadruplewordElement(0x107C)),						
-						m(RealsysCnv.ChannelId.MASS_FLOW, new UnsignedQuadruplewordElement(0x1080)),
-						m(RealsysCnv.ChannelId.VOLUME_FLOW, new UnsignedQuadruplewordElement(0x1084)),
-						m(RealsysCnv.ChannelId.ENERGY_FLOW, new UnsignedQuadruplewordElement(0x1088)),
-						m(RealsysCnv.ChannelId.NORMAL_FLOW, new UnsignedQuadruplewordElement(0x108C))
-						)
+						m(RealsysCnv.ChannelId.PRESSURE, new FloatDoublewordElement(0x107C))),
+				new FC4ReadInputRegistersTask(0x1080, Priority.LOW,
+						m(RealsysCnv.ChannelId.TOTAL_MASS_FLOW, new FloatDoublewordElement(0x1080))),
+				new FC4ReadInputRegistersTask(0x1084, Priority.LOW,
+						m(RealsysCnv.ChannelId.TOTAL_VOLUME_FLOW, new FloatDoublewordElement(0x1084))),
+				new FC4ReadInputRegistersTask(0x1088, Priority.LOW,
+						m(RealsysCnv.ChannelId.TOTAL_ENERGY_FLOW, new FloatDoublewordElement(0x1088))),								
+				new FC4ReadInputRegistersTask(0x108C, Priority.LOW,
+						m(RealsysCnv.ChannelId.TOTAL_NORMAL_FLOW, new FloatDoublewordElement(0x108C)))						
 				);
 	}
 	
+	Channel<Float> getMassFlowRate() {
+		return this.channel(ChannelId.MASS_FLOW_RATE);
+	}	
+	Channel<Float> getVolumeFlowRate() {
+		return this.channel(ChannelId.VOLUME_FLOW_RATE);
+	}	
+	Channel<Float> getEnergyFowRate() {
+		return this.channel(ChannelId.ENERGY_FLOW_RATE);
+	}	
+	Channel<Float> getNormalFlowRate() {
+		return this.channel(ChannelId.NORMAL_FLOW_RATE);
+	}	
 	Channel<Float> getPressure() {
 		return this.channel(ChannelId.PRESSURE);
-	}
-	
-	Channel<Float> getMassFlow() {
-		return this.channel(ChannelId.MASS_FLOW);
-	}
-	
-	Channel<Float> getVolumeFlow() {
-		return this.channel(ChannelId.VOLUME_FLOW);
-	}
-	
-	Channel<Float> getEnergyFow() {
-		return this.channel(ChannelId.ENERGY_FLOW);
-	}
-	
-	Channel<Float> getNormalFlow() {
-		return this.channel(ChannelId.NORMAL_FLOW);
+	}	
+	Channel<Float> getTotalMassFlow() {
+		return this.channel(ChannelId.TOTAL_MASS_FLOW);
+	}	
+	Channel<Float> getTotalVolumeFlow() {
+		return this.channel(ChannelId.TOTAL_VOLUME_FLOW);
+	}	
+	Channel<Float> getTotalEnergyFow() {
+		return this.channel(ChannelId.TOTAL_ENERGY_FLOW);
+	}	
+	Channel<Float> getTotalNormalFlow() {
+		return this.channel(ChannelId.TOTAL_NORMAL_FLOW);
 	}	
 
 	@Override
 	public MeterType getMeterType() { 
 		return this.config.type();
 	}
-
 	@Override
 	public String debugLog() {
-		return "L:" + this.getPressure().value().asString() + " / "
-		+ this.getMassFlow().value().asString() + " / "
-		+ this.getVolumeFlow().value().asString() + " / "
-		+ this.getEnergyFow().value().asString() + " / "
-		+ this.getNormalFlow().value().asString();
+		return "L:" + this.getNormalFlowRate().value().asString() + " / "
+				+ this.getTotalNormalFlow().value().asString() + " / "
+				+ this.getMassFlowRate().value().asString() + " / "
+				+ this.getEnergyFowRate().value().asString() + " / "
+				+ this.getVolumeFlowRate().value().asString();
 	}
 }
